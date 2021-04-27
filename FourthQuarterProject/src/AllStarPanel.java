00e0e66425fab5fe3ce326bb6ef43216a0751398
Dev Kodre <50774710+devkodre@users.noreply.github.com>
2021-04-27 10:57:03 -0400

    Added more parameter descriptions and delete player class

--- FourthQuarterProject/src/AllStarPanel.java
+++ FourthQuarterProject/src/AllStarPanel.java
@@ -1,8 +1,9 @@
+
 import edu.fcps.Turtle;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-//Name: Dev Kodre
+//@Author Dev Kodre
 //Period: 2
 //Date: 4/26/21
 
@@ -11,19 +12,35 @@ import java.awt.event.*;
 * Allows the user to run a simulation based on players selected.
 *
 *
-* @param start_Button
-* @param cancel_Button
-* @param JComboBox Drop down menu
-* @param teamScore_Label
-* @param playerScore_Label
-* @param Directions_Label 
-  
+* @param start_Button  Executes simulation of game between players selected.
+* @param cancel_Button Terminates simulation after start button is pressed.
+* @param JComboBox Drop down menu to select players
+* @param SelectButton   selects the player highlighted in the JComboBox
+* @param team1Score_Label   Displays total score for team one 
+* @param team2Score_Label   Displays total score for team two
+*
+* Displays points scored, scoring ratio for 2 pointers and 3 pointers for each player
+*
+* @param team1p1Stats_Label 
+* @param team1p2Stats_Label
+* @param team1p3Stats_Label
+* @param team1p4Stats_Label
+* @param team1p5Stats_Label
+*
+* @param team2p1Stats_Label
+* @param team2p2Stats_Label
+* @param team2p3Stats_Label
+* @param team2p4Stats_Label
+* @param team2p5Stats_Label
+
+* @param Directions_Label  Information on how to run the simulation
+   
 */
 public class AllStarPanel extends JPanel {
-  
+   
    public AllStarPanel ()
    {
-     
+      
    }
    /**
    * Displays animation during the run of the program.
@@ -33,7 +50,7 @@ public class AllStarPanel extends JPanel {
    */
    public void paintComponent(Graphics g)
    {
-  
+   
    }
    /**
    * Adds number of points scored to player's total number of scored points
@@ -44,9 +61,9 @@ public class AllStarPanel extends JPanel {
    */
    public int PlayerAddPoint(int points )
    {
-     
+      
       return 1;
-   }
+   } 
    /**
    * Adds number of points scored to team's total number of scored points
    * The method will be called after the start button has been pressed.
@@ -56,7 +73,7 @@ public class AllStarPanel extends JPanel {
    */
    public int TeamAddPoint(int points )
    {
-     
+      
       return 1;
    }
    /**
@@ -73,7 +90,7 @@ public class AllStarPanel extends JPanel {
    */
       public void actionPerformed(ActionEvent e)
       {
-           
+            
       }
    }
    /**
@@ -90,7 +107,7 @@ public class AllStarPanel extends JPanel {
    */
       public void actionPerformed(ActionEvent e)
       {
-           
+            
       }
    }
    /**
@@ -107,7 +124,7 @@ public class AllStarPanel extends JPanel {
    */
       public void actionPerformed(ActionEvent e)
       {
-           
+            
       }
    }
    /**
@@ -124,7 +141,24 @@ public class AllStarPanel extends JPanel {
    */
       public void actionPerformed(ActionEvent e)
       {
-           
+            
+      }
+   }
+   /**
+   * removes most recent player selected
+   *
+   *
+   */
+   private class DeletePressed implements ActionListener
+   {
+   /**
+   * Removes player selected from the team
+   *
+   * @param actionEvent e
+   */
+      public void actionPerformed(ActionEvent e)
+      {
+            
       }
    }
 }
