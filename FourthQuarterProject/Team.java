30e3e67775f2609b669d12eb74e2c1bffd2e8112
Dev Kodre <50774710+devkodre@users.noreply.github.com>
2021-05-19 18:23:20 -0400

    added reset players

--- FourthQuarterProject/Team.java
+++ FourthQuarterProject/Team.java
@@ -9,105 +9,112 @@
  * @author Manav Gagvani
 */
 public class Team {
-    private String name;
-    private Player[] players = new Player[5];
-    private int appendIdx = 0;
-    private double pct2PA, pct3PA;
+   private String name;
+   private Player[] players = new Player[5];
+   private int appendIdx = 0;
+   private double pct2PA, pct3PA;
 
-    /**
-     * Constructor for Team. 
-     * Takes the team's name as well as data about the overall tendencies of a team.
-     * 
-     * @param name Name of the team
-     * @param pct2PA The percentage of the team's shot attempts which are inside the three point line
-     * @param pct3PA The percentage of the team's shot attempts which are outside the three point line (Equal to 1- pct2PA)
-     */
-    public Team(String name, double pct2PA, double pct3PA) {
-        assert(1 - pct2PA == pct3PA);
-        this.name = name;
-        this.pct2PA = pct2PA;
-        this.pct3PA = pct3PA;
-    }
-    public Team()
-    {
-    
-    }
+   /**
+    * Constructor for Team. 
+    * Takes the team's name as well as data about the overall tendencies of a team.
+    * 
+    * @param name Name of the team
+    * @param pct2PA The percentage of the team's shot attempts which are inside the three point line
+    * @param pct3PA The percentage of the team's shot attempts which are outside the three point line (Equal to 1- pct2PA)
+    */
+   public Team(String name, double pct2PA, double pct3PA) {
+      assert(1 - pct2PA == pct3PA);
+      this.name = name;
+      this.pct2PA = pct2PA;
+      this.pct3PA = pct3PA;
+   }
+   public Team()
+   {
+   
+   }
 
-    /**
-     * Adds a Player to the Team, specifically the array players. 
-     * 
-     * @param player Player object
-     */
-    public void addPlayer(Player player) {
-        if(appendIdx > 4) {
-            appendIdx = 0;
-        }
-        players[appendIdx] = player;
-        appendIdx += 1;
-    }
+   /**
+    * Adds a Player to the Team, specifically the array players. 
+    * 
+    * @param player Player object
+    */
+   public void addPlayer(Player player) {
+      
+      if(appendIdx > 4) {
+         appendIdx = 0;
+      }
+      players[appendIdx] = player;
+      appendIdx += 1;
+   }
+   public void resetPlayers() {
+      appendIdx = 0;
+      
+      players = new Player [5];
+   }
 
-    /**
-     * Resets the scores of each player in the team to 0.
-     */
-    public void reset_scores() {
-        for(Player player : players) {
-            player.reset_score();
-        }
-    }
 
-    /**
-     * Gets the scores for each player in the team.
-     * The indexing of scores matches with the players array, so scores[0] corresponds with players[0].
-     * 
-     * @return Points scored by each player in an array. 
-     */
-    public int[] get_scores() {
-        int[] scores = new int[players.length];
-        int i = 0;
-        for(Player player: players) {
-            scores[i] = player.get_score();
-            i += 1;
-        }
-        return scores;
-    }
+   /**
+    * Resets the scores of each player in the team to 0.
+    */
+   public void reset_scores() {
+      for(Player player : players) {
+         player.reset_score();
+      }
+   }
 
-    /**
-     * Gets the shooting percentages for each player.
-     * 
-     * @see get_scores() for information on indexing 
-     * @return A 2D array of int, which are the player's shot makes and shot attempts
-     */
-    public int[][] get_shooting_splits() {
-        int[][] splits = {};
-        int i = 0;
-        for(Player player: players) {
-            splits[i] = player.get_splits();
-        }
-        return splits;
-    }
+   /**
+    * Gets the scores for each player in the team.
+    * The indexing of scores matches with the players array, so scores[0] corresponds with players[0].
+    * 
+    * @return Points scored by each player in an array. 
+    */
+   public int[] get_scores() {
+      int[] scores = new int[players.length];
+      int i = 0;
+      for(Player player: players) {
+         scores[i] = player.get_score();
+         i += 1;
+      }
+      return scores;
+   }
 
-    /**
-     * Resets the shooting percentages for each player on the team.
-     */
-    public void reset_splits() {
-        for(Player player: players) {
-            player.reset_splits();
-        }
-    }
+   /**
+    * Gets the shooting percentages for each player.
+    * 
+    * @see get_scores() for information on indexing 
+    * @return A 2D array of int, which are the player's shot makes and shot attempts
+    */
+   public int[][] get_shooting_splits() {
+      int[][] splits = {};
+      int i = 0;
+      for(Player player: players) {
+         splits[i] = player.get_splits();
+      }
+      return splits;
+   }
 
-    /**
-     * Returns an array of the team's players
-     */
-    public Player[] get_players() {
-        return players;
-    }
+   /**
+    * Resets the shooting percentages for each player on the team.
+    */
+   public void reset_splits() {
+      for(Player player: players) {
+         player.reset_splits();
+      }
+   }
 
-    /**
-     * Given a list of players, make those players the team's players
-     * 
-     * @param playerList List of Player objects
-     */
-    public void set_players(Player[] playerList) {
-        players = playerList;
-    }
+   /**
+    * Returns an array of the team's players
+    */
+   public Player[] get_players() {
+      return players;
+   }
+
+   /**
+    * Given a list of players, make those players the team's players
+    * 
+    * @param playerList List of Player objects
+    */
+   public void set_players(Player[] playerList) {
+      players = playerList;
+   }
 }
